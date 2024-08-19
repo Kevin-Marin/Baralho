047b9173ae55c260cb0987ed7b49c3cf19f16ce6
Marin <kuglermarinkevin@gmail.com>
2024-08-19 11:43:07 -0300

    feat: crud de carta

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const naipe_controller = require("./controllers/naipe.js");
 const app = express();
 const port = 3000;
 
+//GERENCIAMENTO DE NAIPES
 app.post("/naipe", (req, res) => {
   const naipe = req.body;
   const code = naipe_controller.store(naipe);
@@ -28,6 +29,34 @@ app.delete("/naipe/:id", (req, res) => {
   naipe_controller.destroy(req.params.id);
   res.json();
 });
+
+// GERENCIAMENTO DE CARTAS
+app.post("/carta", (req, res) => {
+    const carta = req.body;
+    const code = naipe_controller.store(carta);
+    res.status(code).json();
+  });
+  
+  app.get("/carta", (req, res) => {
+    res.json(carta_controller.index());
+  });
+  
+  app.get("/carta/:id", (req, res) => {
+    const carta = naipe_controller.show(req.params.id);
+    res.json(carta);
+  });
+  
+  app.put("/carta/:id", (req, res) => {
+    const carta = req.body;
+    const code = naipe_controller.update(req.params.id, carta);
+    res.status(code).json();
+  });
+  
+  app.delete("/carta/:id", (req, res) => {
+    naipe_controller.destroy(req.params.id);
+    res.json();
+  });
+  
 
 app.listen(port, () => {
   console.log("Gerenciador de baralho executando na porta " + port);
